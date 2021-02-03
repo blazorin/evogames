13f8b21d36ea0009635682485326acf16a5f78de
Alberto <admin@darksnakegames.com>
2021-02-03 21:02:06 +0100

    Configure Model and Client services in application run

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Model.Extensions;
 
 namespace Server
 {
@@ -21,6 +22,8 @@ namespace Server
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddEvoGamesModelServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
