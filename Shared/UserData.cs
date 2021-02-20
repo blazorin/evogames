4653c57871d3a60a993e1b674d45496f7acd1091
Alberto <admin@darksnakegames.com>
2021-02-20 19:26:53 +0100

    Update UserData with Policies stored in Model (UserPerms.cs)

--- Shared/UserData.cs
+++ Shared/UserData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using Shared.Enums;
 
 namespace Shared
@@ -7,6 +8,7 @@ namespace Shared
     {
         public string Username { get; set; }
         public IEnumerable<string> Roles { get; set; }
+        public IEnumerable<Claim> Policies { get; set; }
         public string Token { get; set; }
         public Language Language { get; set; }
     }
