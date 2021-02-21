e586a6e791f301e21583207eb239686b3b151b42
Alberto <32218025+3dsx@users.noreply.github.com>
2021-02-21 01:08:24 +0100

    Update UserCredentials.cs

--- Shared/UserCredentials.cs
+++ Shared/UserCredentials.cs
@@ -4,7 +4,7 @@ namespace Shared
 {
     public class UserCredentials
     {
-        [Required] public string Username { get; set; }
+        [Required] public string Email { get; set; }
         [Required] public string Password { get; set; }
     }
 }
