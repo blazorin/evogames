9876f2a8d01914f10ce396954db6fa79cb8bc23d
Alberto <admin@darksnakegames.com>
2021-02-21 01:07:52 +0100

    Add GetAuthenticationStateAsync method called on initialization

--- Client/Extensions/Authentication/CustomAuthenticationStateProvider.cs
+++ Client/Extensions/Authentication/CustomAuthenticationStateProvider.cs
@@ -22,11 +22,34 @@ namespace Client.Extensions.Authentication
             _jsRuntime = jsRuntime;
         }
 
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        /// <summary>
+        /// Este método se llama automáticamente por AuhtorizeRouteView en la inicialización
+        /// </summary>
+        /// <returns></returns>
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+            var storedUser = await _jsRuntime.InvokeAsync<UserData>("localStorage.getItem", "user");
+
+            if (storedUser == null)
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, storedUser.Username)
+            };
+
+            claims.AddRange(storedUser.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
+            claims.AddRange(storedUser.Policies);
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", storedUser.Token);
+
+            // TODO: Para I18N, establecer el lenguaje preferido contenido en UserData
+
+            return new AuthenticationState(
+                new ClaimsPrincipal(
+                    new ClaimsIdentity(claims, "custom")));
         }
-        
+
         public async Task SetCurrentUserAsync(UserData userData)
         {
             var claims = new List<Claim>
@@ -35,16 +58,17 @@ namespace Client.Extensions.Authentication
             };
             claims.AddRange(userData.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
             claims.AddRange(userData.Policies);
-            
+
             var claimsIdentity = new ClaimsIdentity(claims, "custom");
             var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
             var authState = new AuthenticationState(claimsPrincipal);
-            
+
             NotifyAuthenticationStateChanged(Task.FromResult(authState));
 
             await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "user", userData);
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", userData.Token);
         }
+
         public async Task ClearCurrentUserAsync()
         {
             var anonymousPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
