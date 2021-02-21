84570fe96ccd97bacae07d3bfa8016492af0a8b7
Alberto <admin@darksnakegames.com>
2021-02-21 11:33:37 +0100

    Add IsOwner, IsDeveloper, IsModerator to UserDto.cs

--- Shared/Dto/UserDto.cs
+++ Shared/Dto/UserDto.cs
@@ -6,7 +6,12 @@ namespace Shared.Dto
     public class UserDto : UserProfileDto
     {
         public string UserId { get; set; }
+
+        public bool IsOwner { get; set; }
+        public bool IsDeveloper { get; set; }
         public bool IsAdmin { get; set; }
+        public bool IsModerator { get; set; }
+
         public decimal Balance { get; set; }
         public DateTime CreationDate { get; set; }
         public Language Language { get; set; }
