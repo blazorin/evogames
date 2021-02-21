f3acc51d79bd58a1762ec0c78a6deb3fcc306670
Alberto <32218025+3DSX@users.noreply.github.com>
2021-02-21 14:45:03 +0100

    Add UserServices.cs and Login action in AccountController.cs

--- Server/Controllers/AccountController.cs
+++ Server/Controllers/AccountController.cs
@@ -4,9 +4,12 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Model.Data;
+using Model.Services;
+using Shared;
 
 namespace Server.Controllers
 {
@@ -14,6 +17,53 @@ namespace Server.Controllers
     [Route("account")]
     public class AccountController : ControllerBase
     {
+        private readonly IUserServices _userServices;
+
+        public AccountController(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserCredentials credentials)
+        {
+            if (credentials == null)
+                return Unauthorized();
+
+            if (User?.Identity != null && User.Identity.IsAuthenticated)
+                return Forbid();
+
+            var user = await _userServices.GetUserByAuthenticationAsync(credentials);
+            if (user == null)
+                return Unauthorized();
+
+            // TODO: Verificaciones de seguridad adicionales
+
+            var policies = new List<Claim>();
+            var roles = new List<string>();
+            policies.AddRange(user.Perms.Select(perm => new Claim(perm.PermKey, perm.PermValue)));
+
+            if (user.IsOwner)
+                roles.Add("owner");
+            if (user.IsDeveloper)
+                roles.Add("developer");
+            if (user.IsAdmin)
+                roles.Add("admin");
+            if (user.IsModerator)
+                roles.Add("moderator");
+
+            var response = new UserData
+            {
+                Username = user.Name,
+                Language = user.Language,
+                Policies = policies,
+                Roles = roles,
+                Token = GenerateToken(user, roles, policies)
+            };
+
+            return Ok(response);
+        }
+
         private string GenerateToken(User user, IEnumerable<string> roles, IEnumerable<Claim> policies)
         {
             var header = new JwtHeader(
