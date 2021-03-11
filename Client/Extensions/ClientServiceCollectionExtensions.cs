76080128dd350eea565ba30f2444b7a649053e0e
Alberto <32218025+3DSX@users.noreply.github.com>
2021-03-11 15:25:21 +0100

    Update ClientServiceCollectionExtensions.cs with Mud configuration

--- Client/Extensions/ClientServiceCollectionExtensions.cs
+++ Client/Extensions/ClientServiceCollectionExtensions.cs
@@ -3,6 +3,8 @@ using Client.Components.Bootstrap;
 using Client.Extensions.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace Client.Extensions
 {
@@ -12,6 +14,22 @@ namespace Client.Extensions
         {
             services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
             services.AddSingleton<BootstrapServices>();
+
+            // External (NuGet)
+            services.AddMudServices(config =>
+                {
+                    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+
+                    config.SnackbarConfiguration.PreventDuplicates = false;
+                    config.SnackbarConfiguration.NewestOnTop = false;
+                    config.SnackbarConfiguration.ShowCloseIcon = true;
+                    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+                    config.SnackbarConfiguration.HideTransitionDuration = 500;
+                    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+                    config.SnackbarConfiguration.MaxDisplayedSnackbars = 5;
+                }
+            );
             services.AddBlazoredLocalStorage();
             return services;
         }
