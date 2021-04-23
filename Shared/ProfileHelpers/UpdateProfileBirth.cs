1cd6330717dc848d96774507f9ff6a319c25d0e0
Alberto <32218025+3DSX@users.noreply.github.com>
2021-04-23 11:07:22 +0200

    Add UpdateProfileBirth object to send new birth to API

---
+++ Shared/ProfileHelpers/UpdateProfileBirth.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Shared.ProfileHelpers
+{
+    public class UpdateProfileBirth
+    {
+        public DateTime? Birth { get; }
+
+        public UpdateProfileBirth(DateTime? birth)
+        {
+            Birth = birth;
+        }
+    }
+}
