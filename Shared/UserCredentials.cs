84c34f1947fe4ad401d9455a59dbc7437f43d0ee
Alberto <32218025+3DSX@users.noreply.github.com>
2021-04-23 10:52:50 +0200

    Remove DataAnottations in UserCredentials.cs as they are checked in AccountController.cs always.

--- Shared/UserCredentials.cs
+++ Shared/UserCredentials.cs
@@ -6,7 +6,6 @@ namespace Shared
 {
     public class UserCredentials
     {
-        [RequiredField, FieldLength(FieldLenghts.User.Mail), EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
         public string Password { get; set; }
