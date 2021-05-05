0d61b51c4f224fcb318d0aebf14f19816f937035
Alberto <32218025+3DSX@users.noreply.github.com>
2021-05-05 15:59:58 +0200

    Add UpdateProfileCountry DTO

---
+++ Shared/ProfileHelpers/UpdateProfileCountry.cs
@@ -0,0 +1,12 @@
+namespace Shared.ProfileHelpers
+{
+    public class UpdateProfileCountry
+    {
+        public string Country { get; }
+
+        public UpdateProfileCountry(string country)
+        {
+            Country = country;
+        }
+    }
+}
