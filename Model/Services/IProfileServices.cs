80a8ee5bf746d35e2a3245e73b6963077b98e10c
Alberto <32218025+3DSX@users.noreply.github.com>
2021-07-01 05:10:24 +0200

    Add Task<bool> UpdateEmail method to Interface.

--- Model/Services/IProfileServices.cs
+++ Model/Services/IProfileServices.cs
@@ -9,7 +9,7 @@ namespace Model.Services
         Task<UserProfileDto> GetProfile(string id);
         Task<bool> UpdateBirth(string id, DateTime? newBirth);
         Task<bool> UpdateCountry(string id, string country);
-
         Task<bool> UpdateUsername(string id, string username);
+        Task<bool> UpdateEmail(string id, string email);
     }
 }
