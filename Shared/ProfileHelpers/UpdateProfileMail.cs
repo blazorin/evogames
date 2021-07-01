3971baae38eb741efe31f910390f73c2e87ac1c8
Alberto <32218025+3DSX@users.noreply.github.com>
2021-07-01 05:08:17 +0200

    Add UpdateProfileMail dto

---
+++ Shared/ProfileHelpers/UpdateProfileMail.cs
@@ -0,0 +1,9 @@
+namespace Shared.ProfileHelpers
+{
+    public class UpdateProfileMail
+    {
+        public string Email { get; }
+
+        public UpdateProfileMail(string email) => Email = email;
+    }
+}
